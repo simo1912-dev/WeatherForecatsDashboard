aaebd57185d86fb727b98cd2c6a928b3aea4b7a4
simo <simoaboulfath52@gmail.com>
2025-08-07 11:02:26 +0200

    Backend Code Developement + filtering Logic

---
+++ backend.py
@@ -0,0 +1,22 @@
+from http.client import responses
+
+import requests
+
+
+API_KEY = "7a620e8b7044df0aefbdfee6a6591e48"
+
+def get_data(place, forecast_days=None, type=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[nr_values:]
+    if type == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if type == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3, type="Temperature"))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for te next Days")
 place = st.text_input("Place:")
@@ -8,3 +9,9 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"Forecast of {option} for the next {days} days in  {place}")
+
+d, t = get_data(place, days, option)
+
+
+figure = px.line(x=dates, y=temperatures, labels=dict(x="Date", y="Temperature"))
+st.plotly_chart(figure)
